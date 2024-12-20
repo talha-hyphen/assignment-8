595ca50860beabbf9f67fb979b958810ca622eac
talha-hyphen <w00052207@gmail.com>
2024-12-20 11:26:01 -0800

    Assignment08

---
+++ script.js
@@ -0,0 +1,147 @@
+//1. Write a js program to find maximum between two numbers.
+//var num1 = prompt ("Enter a 1st Number");
+//var num2 = prompt ("Enter a 2nd Number");
+//if(num1>=0|| num1<=0 || num2>=0|| num2<=0 ){
+ //if (num1 > num2) { alert ("1st number is greater 2nd")}
+ // else if(num1 < num2) { alert ("2nd number is greater 1st")}
+//else if(num1 == num2) {alert ("1st number = 2nd number")}}
+//else{alert("Enter a Number please.")}
+
+
+
+
+
+//2. Write a js program to find maximum between three numbers.
+//var number1 = prompt("Enter 1st Number")
+//var number2 = prompt("Enter 2nd Number")
+//var number3 = prompt("Enter 3rd Number")
+//if (number1>number2 && number1>number3) {
+ //   alert("1st Number is Greater")
+//}
+//else if (number1<number2 && number2>number3) {
+  //  alert("2nd Number is Greater")
+//}else if (number1<number3 && number2<number3) {
+ //   alert("3rd Number is Greater")
+//}
+//else if (number2==number1==number3){
+ //   alert("1st number = 2nd number = 3rd number  ")
+//}
+
+
+
+
+
+
+//3. Write a js program to check whether a number is negative, positive or zero.
+//var userInput = prompt("Enter to Check a Number.")
+//if(userInput>=0 || userInput<=0){
+//if (userInput<0) {
+ //   alert("Number is Negative.")
+//}
+//else if (userInput>0) {
+ //   alert("Number is Postive.")
+//}else if (userInput==0) {
+ //   alert("Number is Zero.")
+//}}
+//else{alert("Enter a Number plaese.")}
+
+
+
+
+//4. Write a js program to check whether a number is divisible by 5 and 11 or not.
+//var userNumber = prompt("Enter a Number to check whether divisible by 5 or 11.")
+//if (userNumber%5==0) {
+//   alert("Number is divisible by '5'.")
+//}
+//else if (userNumber%11==0) {
+//    alert("Number is divisible by '11'.")
+//}
+//else if (userNumber%5 !=0||userNumber%11 !=0) {
+//    alert("Number is neither divisible by '5' and '11'. ")
+//}
+
+
+
+
+
+//5. Write a js program to check whether a number is even or odd.
+//var NUMBER = prompt("Enter a Number to Check either Even or Odd. ")
+//if (NUMBER % 2 == 0) {
+ //   alert("Number is Even.")
+//}
+//else if (NUMBER % 2 != 0){alert("Number is Odd.")}
+
+
+
+
+
+
+//6. Write a js program to check whether a year is leap year or not.a js program to check whether a character is alphabet or not.
+//var leapYear = prompt("Enter a year to check it whether is it Leap year or not.")
+//if (leapYear%4 == 0)
+//{  alert("This is a Leap year.")}
+//else 
+//{alert("This is not a Leap years")}
+
+
+
+
+
+//14. Write a js program to input angles of a triangle and check whether triangle is valid or not.
+//var triangle = alert("Enter three angles to check, Triangle is valid or not.")
+//var a =  parseInt(prompt("Enter First anlge."))
+//var b =  parseInt(prompt("Enter Second angle."))
+//var c = parseInt(prompt("Enter Third angle."))
+//var abc = a+b+c
+//if(a>0 && b>0 && c>0){
+ //if (abc === 180) {
+//alert("Triangle is Valid.")
+//}
+//else  {
+  //alert("Trianlge is not Valid.")
+//}
+//}
+//else  {
+ // alert("Trianlge is not Valid.")
+//}
+
+
+
+//15. Write a js program to input all sides of a triangle and check whether triangle is valid or not.
+//var sideTri = alert("Enter three side of tiangle and Check whether a triangle is Valid or not.")
+//var s1 = parseInt(prompt("Enter 1st side."))
+//var s2 = parseInt(prompt("Enter 2nd side."))
+//var s3 = parseInt(prompt("Enter 3rd side."))
+//if (s1>0 && s2>0 && s3>0) {
+  //if (s1+s2>s3 && s2+s3>s1 && s3+s1>s2) {
+  // alert("Triangle is Valid.")}
+  // else{alert("Triangle is not valid.")}
+//}
+//else{alert("Triangle is not valid.")}
+
+
+
+
+//16. Write a js program to check whether the triangle is equilateral, isosceles or scalene triangle.
+
+//var triAngle = alert("Check Triangle is either equilateral, isosceles or scalene triangle. ")
+//var Ang1 = prompt("Enter Length of 1st side.")
+//var Ang2 = prompt("Enter Length of 2nd side.")
+//var Ang3 = prompt("Enter Length of 3rd side.")
+//if(Ang1>0 && Ang2>0 && Ang3>0)
+//{
+//if (Ang1==Ang2==Ang3)
+//{
+ // alert("Triangle is Eqilateral.")
+//}
+//else if(Ang1==Ang2||Ang2==Ang3||Ang3==Ang1)
+//{alert("Triangle is Isoscele.")}
+//else if(Ang1 !=Ang2 && Ang2 !=Ang3 &&Ang3 !=Ang1)
+//{alert("Trianlge is Scalene.")}
+//}
+//else{alert("Tianlge does not exist.")}
+
+
+
+
+ 
